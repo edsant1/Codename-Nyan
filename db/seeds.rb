f31752c41aca0edadde8a687843ebf6aac67189b
erikb <erikburton1@gmail.com>
2014-09-13 13:41:36 -0600

    creates seed for articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.create[{name: 'v-neck', description: 'A low cut t-shirt.', type: 'shirt'},
+							 {name: 't-shirt', description: 'A standard t-shirt.', type: 'shirt'},
+							 {name: 'button-down', description: 'A shirt with buttons down the front.', type: 'shirt',
+							 {name: 'slacks', description: 'Dress pants.', type: 'pants'},
+							 {name: 'jeans', description: 'Standard jeans.', type: 'pants'},
+							 {name: 'shorts', description: 'Standard shorts.', type: 'pants'},
+							 {name: 'loafers', description: 'Casual slip on shoes.', type: 'shoes'},
+							 {name: 'dress', description: 'Standard Dress shoes.', type: 'shoes'},
+							 {name: 'sneakers', description: 'Athletic shoes.', type: 'shoes'}]
+							 
